c613337807ffc1f19885211e9b72e234beaec826
yann300 <yann.levreau@gmail.com>
2024-09-25 17:26:57 +0200

    Update worker_chain.ts

--- client/src/worker_chain.ts
+++ client/src/worker_chain.ts
@@ -1,4 +1,4 @@
-import Ganache, { EthereumProvider } from "ganache";
+import { Provider } from "@remix-project/remix-simulator";
 
 function onConfigure(evt: MessageEvent): void {
     removeEventListener("message", onConfigure);
@@ -6,20 +6,20 @@ function onConfigure(evt: MessageEvent): void {
     // Without doing runtime type checking, it's impossible to ensure the
     // message data conforms to the options type from ganache.
     // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-    const ganache = Ganache.provider(evt.data);
-    addEventListener("message", (evt) => onMessage(ganache, evt));
+    const provider = new Provider({ fork: 'cancun' })
+    addEventListener("message", (evt) => onMessage(provider, evt));
 }
 
-function onMessage(ganache: EthereumProvider, evt: MessageEvent): void {
+function onMessage(provider: any, evt: MessageEvent): void {
     const reply = evt.ports[0];
-    ganache
+    provider
         // Without doing runtime type checking, it's impossible to ensure the
         // message data conforms to the JSON RPC spec.
         // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
         .request(evt.data)
         .then((result) => reply.postMessage({ result }))
         .catch((error) => {
-            console.error("Uncaught (in ganache worker thread)", error);
+            console.error("Uncaught (in remix-simulator worker thread)", error);
             reply.postMessage({
                 error: {
                     message: String(error),
